052d4b1f552b0ac29beac5e866c62667c368f0ed
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-10-07 09:30:01 -0300

    drying

--- desafio/adicoes.py
+++ desafio/adicoes.py
@@ -5,7 +5,6 @@ from typing import List
 
 def add_imcs(data: List[dict]) -> List[dict]:
     data = deepcopy(data)
-#def add_imcs():
     """ 
         Muito abaixo do peso:  menor que 17 kg/m²
         Abaixo do Peso: Entre 17 kg/m² e 18.4 kg/m²
@@ -14,12 +13,9 @@ def add_imcs(data: List[dict]) -> List[dict]:
         Obesidade: Acime de 35 kg/m² 
     """
 
-    #for index, _ in enumerate (data['pessoas']):
     for pessoa in data:
 
-        #weight = data['pessoas'][index]['peso']
         weight = pessoa['peso']
-        #height = data['pessoas'][index]['altura']
         height = pessoa['altura']
         
         height_in_meters = height/ 100
@@ -36,32 +32,23 @@ def add_imcs(data: List[dict]) -> List[dict]:
         else:
             pessoa['imc'] = 'Obesidade'
 
-    #return {}
     return data
-
-#print(add_imcs(List[dict]))
 
 
 def add_nome_completo(data: List[dict]) -> List[dict]:
     data = deepcopy(data)
-#def add_nome_completo():
 
-    #for index, _ in enumerate (data['pessoas']):
     for pessoa in data:
 
-        #Create nome_completo atribute
         pessoa['nome_completo'] = pessoa['nome']+ " " + pessoa['sobrenome'] 
-        # delete nome and sobrenome atribute
         del pessoa['nome'],pessoa['sobrenome']
 
-    #return {}
     return data
 
 
 def add_idade(data: List[dict]) -> List[dict]:
     data = deepcopy(data)
-#def add_idade():
-    
+
     for pessoa in data:
 
         birth_timestamp = pessoa['nascimento']
@@ -73,4 +60,3 @@ def add_idade(data: List[dict]) -> List[dict]:
         pessoa['idade'] = int(year[0])
         
     return data
-    #return {}
