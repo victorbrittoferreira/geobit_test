b5015780cf35f52479a5e00ee5259b25a899b070
victorbrittoferreira <victorbrittoferreira@gmail.com>
2021-10-08 08:36:40 -0300

    escrita updata

--- desafio/escrita.py
+++ desafio/escrita.py
@@ -7,7 +7,7 @@ def data_to_json(pessoas: List[dict], filter_1: List[dict], filter_2: List[dict]
     json_data = {
             "pessoas": [],
             "filtros": {
-                "menores_de_idade_com_imc_acima_peso": [],
+                "maiores_de_idade_com_imc_acima_peso": [],
                 "mulheres_meeren_braavos": []
             }
         }
@@ -16,13 +16,13 @@ def data_to_json(pessoas: List[dict], filter_1: List[dict], filter_2: List[dict]
         json_data['pessoas'].append(pessoa)
 
     for pessoa in filter_1:
-        json_data['filtros']['menores_de_idade_com_imc_acima_peso'].append(pessoa)
+        json_data['filtros']['maiores_de_idade_com_imc_acima_peso'].append(pessoa)
 
     for pessoa in filter_2:
         json_data['filtros']['mulheres_meeren_braavos'].append(pessoa)
 
 
-    with open('/home/kurt/GitHub/geobit_test/output.json', 'w') as fp:
-        output = json.dump(json_data, fp ,ensure_ascii=False, indent=4)
+    with open('/home/kurt/GitHub/geobit_test/output.json', 'w') as json_data:
+        json.dump(json_data, ensure_ascii= False, indent= 4)
 
     
